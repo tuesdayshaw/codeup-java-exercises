234286ff4010043c8c5fb41616abda8e6531e7e8
Tuesday Shaw <tuesday.shaw@gmail.com>
2017-05-09 18:42:03 -0500

    all loop basic exercises done

--- src/exercises/ControlFlowExercises.java
+++ src/exercises/ControlFlowExercises.java
@@ -1,5 +1,7 @@
 package exercises;
 
+import java.math.BigInteger;
+
 /**
  * Created by canidmars on 5/9/17.
  */
@@ -13,11 +15,50 @@ public class ControlFlowExercises {
 //            i++;
 //        }
 
-        Integer i = 0;
 
-        do{
-            System.out.println(i += 2);
-        }while (i < 100);
+
+//        Integer i = 0;
+//
+//        do{
+//            System.out.println(i += 2);
+//        }while (i < 100);
+
+
+
+//        Integer i = 100;
+//
+//        do {
+//            System.out.println(i -= 5);
+//        }while (i > -10);
+
+
+
+//        long i = 2;
+//
+//        do {
+//            i = i*i;
+//            System.out.println(i);
+//        }while (i < 1000000);
+
+
+//        for(int i = 5; i <= 15; i++){
+//            System.out.println(i);
+//        }
+
+
+//        for(int i = 0; i <= 100; i+=2){
+//            System.out.println(i);
+//        }
+
+
+//        for(int i = 100; i >= -10; i-=5){
+//            System.out.println(i);
+//        }
+
+
+//        for(long i = 2; i < 1000000; i = i*i){
+//            System.out.println(i);
+//        }
     }
 
 }
