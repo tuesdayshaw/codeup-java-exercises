8d576101a3aa2a2c8bccb1ab59d467852dc20515
Tuesday Shaw <tuesday.shaw@gmail.com>
2017-05-09 15:40:57 -0500

    do while loop commit

--- src/exercises/ControlFlowExercises.java
+++ src/exercises/ControlFlowExercises.java
@@ -6,12 +6,18 @@ package exercises;
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-        Integer i = 5;
+//        Integer i = 5;
 
-        while (i <= 15){
-            System.out.println(i);
-            i++;
-        }
+//        while (i <= 15){
+//            System.out.println(i);
+//            i++;
+//        }
+
+        Integer i = 0;
+
+        do{
+            System.out.println(i += 2);
+        }while (i < 100);
     }
 
 }
