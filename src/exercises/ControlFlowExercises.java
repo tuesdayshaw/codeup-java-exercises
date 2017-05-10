1221fc09738200ba10b3ebd5ca3d55ee777d8c3b
Tuesday Shaw <tuesday.shaw@gmail.com>
2017-05-10 11:58:32 -0500

    added bonus exercises to code (commented out) to start working on

--- src/exercises/ControlFlowExercises.java
+++ src/exercises/ControlFlowExercises.java
@@ -68,7 +68,7 @@ public class ControlFlowExercises {
 //        Part Two:
 
 //        for(int i=0; i<100; i++){
-//            if (i % 3 == 0 & i % 5 == 0){
+//            if (i % 3 == 0 && i % 5 == 0){
 //                System.out.println("FizzBuzz");
 //            } else if (i % 5 == 0){
 //                System.out.println("Buzz");
@@ -101,7 +101,7 @@ public class ControlFlowExercises {
                 String a = String.valueOf(i++);
                 String b = String.valueOf(i*i);
                 String c = String.valueOf(i*i*i);
-                System.out.println(String.format(c.length() - a.length()));
+                System.out.printf("%c.length() - a.length()s", a, b, c);
             } while (i < enteredNumber);
         }
 
@@ -149,6 +149,38 @@ public class ControlFlowExercises {
 //
 //        }
 
+
+//        Bonus One:
+
+//        Convert a number to a string, the contents of which depend on the number's factors.
+//
+//        If the number has 3 as a factor, output 'Pling'.
+//                If the number has 5 as a factor, output 'Plang'.
+//                If the number has 7 as a factor, output 'Plong'.
+//                If the number does not have 3, 5, or 7 as a factor, just pass the number's digits straight through.
+//
+//        Examples
+//
+//        28's factors are 1, 2, 4, 7, 14, 28.
+//        In raindrop-speak, this would be a simple "Plong".
+//        30's factors are 1, 2, 3, 5, 6, 10, 15, 30.
+//        In raindrop-speak, this would be a "PlingPlang".
+//        34 has four factors: 1, 2, 17, and 34.
+//        In raindrop-speak, this would be "34".
+//
+//        Bonus points if you let the user decide when to stop translating to raindrop. (do-while)
+
+
+
+
+
+//        Bonus Two:
+//
+//        Suppose you have a set of grades (a number between 0 and 100) of a group
+//        of 10 students. Write a program to calculate the average score and the
+//        lowest score for the entire group.
+//
+//                What loop would be better? while, do-while or a for?
     }
 
 }
