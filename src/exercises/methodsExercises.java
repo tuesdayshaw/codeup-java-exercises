fcfd0fa4ff87e26c5d2dc6c9b72bbd2f852ab2ab
Tuesday Shaw <tuesday.shaw@gmail.com>
2017-05-11 15:37:13 -0500

    finished basic arithmetic methods

---
+++ src/exercises/methodsExercises.java
@@ -0,0 +1,45 @@
+package exercises;
+
+import java.util.Scanner;
+
+/**
+ * Created by canidmars on 5/11/17.
+ */
+public class methodsExercises {
+
+    Scanner input = new Scanner(System.in);
+
+    public static int addition(int number1, int number2){
+        int addNumbers = number1 + number2;
+        return addNumbers;
+    }
+
+    public static int subtraction(int number1, int number2){
+        int minusNumbers = number1 - number2;
+        return minusNumbers;
+    }
+
+    public static int multiplication(int number1, int number2){
+        int timesNumbers = number1 * number2;
+        return timesNumbers;
+    }
+
+    public static int division(int number1, int number2){
+        int divideNumbers = number1 / number2;
+        return divideNumbers;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(addition(10,30));
+        System.out.println(subtraction(44, 67));
+        System.out.println(multiplication(8, 90));
+        System.out.println(division(20,5));
+    }
+
+
+
+
+
+
+
+}
