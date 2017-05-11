1d78512cc62a270dfa1c7d60c70e2f268d32f1b3
Tuesday Shaw <tuesday.shaw@gmail.com>
2017-05-11 10:35:00 -0500

    finished additional exercise in string manipulation

--- src/exercises/stringExercise.java
+++ src/exercises/stringExercise.java
@@ -1,5 +1,7 @@
 package exercises;
 
+import java.util.Scanner;
+
 /**
  * Created by canidmars on 5/11/17.
  */
@@ -18,9 +20,41 @@ public class stringExercise {
 //
 //        System.out.println(message);
 
-        String message = "I can do backslashes \\, double backslashes \\\\,\n" +
-                "and the amazing triple backslash \\\\\\!";
+//        String message = "I can do backslashes \\, double backslashes \\\\,\n" +
+//                "and the amazing triple backslash \\\\\\!";
+//
+//        System.out.println(message);
 
-        System.out.println(message);
+
+//        Additional Exercise:
+//        Bob is a lackadaisical teenager. In conversation, his responses are very limited.
+//
+//        Bob answers 'Sure.' if you ask him a question.
+//
+//        He answers 'Whoa, chill out!' if you yell at him.
+//
+//        He says 'Fine. Be that way!' if you address him without actually saying anything.
+//
+//        He answers 'Whatever.' to anything else.
+
+
+        String message1 = "Do you like pizza?";
+        String message2 = "GO CLEAN YOUR ROOM!";
+        String message3 = "     ";
+
+        if (message1.endsWith("?")){
+            System.out.println("Sure.");
+        }
+
+        if (message2.toUpperCase().equals(message2)){
+            System.out.println("Whoa, chill out!");
+        }
+
+        if (message3.trim().equals("")){
+            System.out.println("Fine, be that way!");
+        }
+
+
+
     }
 }
