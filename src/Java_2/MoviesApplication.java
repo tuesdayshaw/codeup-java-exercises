4a55ed5592994c981d2b3618c59566117dafb0a6
Tuesday Shaw <tuesday.shaw@gmail.com>
2017-05-18 16:55:43 -0500

    working on bonus (user adding movie) incomplete

--- src/Java_2/MoviesApplication.java
+++ src/Java_2/MoviesApplication.java
@@ -1,5 +1,6 @@
 package Java_2;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -9,28 +10,32 @@ public class MoviesApplication {
 
     public static void main(String[] args) {
 
-
         Input input = new Input(new Scanner(System.in));
 
         System.out.println("Would you like to see the movies?");
         String confirm = input.getString();
 
         while (confirm.equalsIgnoreCase("y")){
+
             System.out.println("What would you like to do?");
             System.out.println("        0 - exit\n" +
                     "        1 - view all movies\n" +
                     "        2 - view movies in the animated category\n" +
                     "        3 - view movies in the drama category\n" +
                     "        4 - view movies in the horror category\n" +
-                    "        5 - view movies in the scifi category");
+                    "        5 - view movies in the scifi category\n" +
+                    "        6 - add a movie, provide name and category");
             System.out.println("Enter your choice: ");
 
-            switch (input.getInt()) {
+            int selection = input.getInt();
+            input.getInt();
+
+            switch (selection) {
                 case 0:
                     System.exit(0);
                     break;
                 case 1:
-                    for (Movie movieListAllTitles : MoviesArray.findAll()) {
+                    for (Movie movieListAllTitles : ) {
                         System.out.println(movieListAllTitles.getName());
                     }
                     break;
@@ -46,6 +51,16 @@ public class MoviesApplication {
                 case 5:
                     printByCategory(MoviesArray.findAll(), "scifi");
                     break;
+                case 6:
+                    System.out.println("Enter the name of the movie:");
+                    String name = input.getString();
+
+                    System.out.println("Enter the name of the category:");
+                    String category = input.getString();
+
+                    Movie newMovie = new Movie(name, category);
+
+                    MoviesArray = Arrays.copyOf(MoviesArray.findAll(), MoviesArray.findAll()+1);
                 default:
                     System.out.println("Not a valid option");
                     break;
@@ -60,5 +75,9 @@ public class MoviesApplication {
                 System.out.println(m.getName());
             }
         }
+    }
+
+    public static void addMovie(){
+
     }
 }
